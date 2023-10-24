21aaadd52f99ec99165915270e7cc9848e6924cb
rob <rgw20@ic.ac.uk>
2023-10-24 14:16:14 +0100

    Add pseudocode decision_tree_learning func

--- decision_tree.py
+++ decision_tree.py
@@ -9,3 +9,17 @@ print(clean_data)
 noisy_data = np.loadtxt("wifi_db/noisy_dataset.txt")
 print("noisy")
 print(noisy_data)
+
+
+def decision_tree_learning(training_dataset, depth):
+    # Check if all samples have the same label
+    # TODO: Can len(training_dataset) == 0 ever?
+    if len(training_dataset) == 0 or all(s.label == training_dataset[0].label for s in training_dataset):
+        return (Leaf(training_dataset[0].value), depth)
+    else:
+        split = find_split(training_dataset)
+        node = DTree(split)
+        l_branch, r_branch = split_dataset(training_dataset, split)
+        l_branch, l_depth = decision_tree_learning(l_dataset, depth+1)
+        r_branch, r_depth = decision_tree_learning(r_dataset, depth+1)
+        return (node, max(l_depth, r_depth))
