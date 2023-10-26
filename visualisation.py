b57acc83f7acbb6a2b0beceed8513e3efbebe05a
rob <rgw20@ic.ac.uk>
2023-10-26 13:17:21 +0100

    Color based on attributes and labels

--- visualisation.py
+++ visualisation.py
@@ -33,24 +33,32 @@ def plot_decision_tree(tree, depth, fname):
     plt.show()
     plt.savefig(output_path + fname + ".png")
 
+def get_attr_color(attr):
+    color_list = ['bisque', 'limegreen', 'mediumslateblue', 'mistyrose', 'paleturquoise', 'plum', 'peachpuff']
+    return color_list[int(attr)]
+
+def get_label_color(label):
+    color_list = ['bisque', 'limegreen', 'mediumslateblue', 'mistyrose', 'paleturquoise', 'plum', 'peachpuff']
+    return color_list[int(label)]
+
 def plot_subtree(tree, x, depth, ax):
     if is_leaf_node(tree):
         # Plot a Leaf Label
         ax.text(x, depth, "Label: " + str(tree['value']),
                 fontsize=6,
-                bbox = dict(facecolor='wheat', alpha=1, boxstyle='round'),
+                bbox = dict(facecolor=get_label_color(tree['value']), alpha=1, boxstyle='round'),
                 ha='center', va='center')
     else:
         # Plot a Node Label
         ax.text(x, depth, "X" + str(tree['attribute']) + " > " + str(tree['value']),
                 fontsize=6,
-                bbox = dict(facecolor='wheat', alpha=1, boxstyle='round'),
+                bbox = dict(facecolor=get_attr_color(tree['attribute']), alpha=1, boxstyle='round'),
                 ha='center', va='center')
         # Plot 2 lines L and R
         l = x-(30/(depth+1)) - 4
         r = x+(30/(depth+1)) + 4
-        plt.plot(np.array([x, l]), np.array([depth, depth+1]))
-        plt.plot(np.array([x, r]), np.array([depth, depth+1]))
+        plt.plot(np.array([x, l]), np.array([depth, depth+1]), 'b')
+        plt.plot(np.array([x, r]), np.array([depth, depth+1]), 'r')
         plot_subtree(tree['left'], l,  depth + 1, ax)
         plot_subtree(tree['right'], r, depth + 1, ax)
 
