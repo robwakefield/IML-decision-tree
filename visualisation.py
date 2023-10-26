a271a15dcb4651df997343cd091cac8ca244c6c3
rob <rgw20@ic.ac.uk>
2023-10-26 12:44:37 +0100

    Recursively plot nodes and leaves. No spacing

--- visualisation.py
+++ visualisation.py
@@ -14,7 +14,7 @@ def plot_decision_tree(tree, depth, fname):
     fig, ax = plt.subplots()
 
     ax.set_ylim(depth, 0)
-    ax.set_xlim(-depth/2, depth/2)
+    ax.set_xlim(-3 * depth, 3 * depth)
 
     # Move left y-axis and bottom x-axis to centre, passing through (0,0)
     ax.spines['left'].set_position('zero')
@@ -26,35 +26,33 @@ def plot_decision_tree(tree, depth, fname):
     ax.spines['top'].set_color('none')
     ax.spines['bottom'].set_color('none')
 
-
+    plot_subtree(tree, 0, 0, ax)
 
     plt.show()
-    return
+    plt.savefig(output_path + fname + ".png")
 
+def plot_subtree(tree, x, depth, ax):
     if is_leaf_node(tree):
         # Plot a Leaf Label
-        ax.text(0, 0, "Label: " + tree['value'],
+        ax.text(x, depth, "Label: " + str(tree['value']),
+                fontsize=6,
                 bbox = dict(facecolor='wheat', alpha=1, boxstyle='round'),
                 ha='center', va='center')
-        return
     else:
         # Plot a Node Label
-        ax.text(0, 0, tree['attribute'] + " > " + tree['value'],
+        ax.text(x, depth, "X" + str(tree['attribute']) + " > " + str(tree['value']),
+                fontsize=6,
                 bbox = dict(facecolor='wheat', alpha=1, boxstyle='round'),
                 ha='center', va='center')
         # Plot 2 lines L and R
-        plot_subtree(tree['left'])
-        plot_subtree(tree['right'])
-
-    plt.savefig(output_path + fname + ".png")
+        plot_subtree(tree['left'], x-30/(depth+1),  depth + 1, ax)
+        plot_subtree(tree['right'], x+30/(depth+1), depth + 1, ax)
 
 
-def plot_subtree(tree, depth, ax):
-    plt.plot(x, y)
     return
 
 if __name__ == "__main__":
     tree = {}
     with open("tree.pkl", "rb") as file:
         tree = pickle.load(file)
-    plot_decision_tree(tree, 2, "test")
+    plot_decision_tree(tree, 14, "test")
