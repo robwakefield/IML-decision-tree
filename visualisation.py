e18bf090ce3d6aab24df3529485bbac714c7e6a7
rob <rgw20@ic.ac.uk>
2023-10-26 20:30:01 +0100

    Update spacing to be based on depth of subtrees

--- visualisation.py
+++ visualisation.py
@@ -6,6 +6,9 @@ import pickle
 
 output_path = "output/"
 
+# Child Count Tree Node
+# { 'value', 'left', 'right' }
+
 # Checks if Tree Node is a Leaf Node
 def is_leaf_node(node):
     return node['left'] == node['right'] == None
@@ -18,7 +21,27 @@ def get_label_color(label):
     color_list = ['bisque', 'limegreen', 'mediumslateblue', 'mistyrose', 'paleturquoise', 'plum', 'peachpuff']
     return color_list[int(label)]
 
+def subtree_depths(tree):
+    if is_leaf_node(tree):
+        return {
+                'left' : None,
+                'right' : None,
+                'value' : 1
+                }
+    else:
+        ltree = subtree_depths(tree['left'])
+        rtree = subtree_depths(tree['right'])
+        return {
+                'left' : ltree,
+                'right' : rtree,
+                'value' : max(ltree['value'], rtree['value']) + 1
+                }
+
 def plot_decision_tree(tree, depth, fname):
+    # First form a new tree of similar dimensions where each node contains the depth of that subtree
+    depth_tree = subtree_depths(tree)
+
+    # Setup matplotlib
     fig = plt.figure(figsize=(50, 10))
     ax = plt.subplot(1, 1, 1)
 
@@ -44,42 +67,46 @@ def plot_decision_tree(tree, depth, fname):
     ax.set_xticks([])
     ax.set_yticks([])
 
-    plot_subtree(tree, min_width, depth, 0, 0, ax)
+    # Call recursive helper function
+    plot_subtree(tree, depth_tree, width/40, width/2, 0, 0, ax)
 
     #plt.show()
     plt.savefig(output_path + fname + ".png")
 
-def plot_subtree(tree, min_width, max_depth, x, current_depth, ax):
-    font_size = 6
-    if current_depth < 3 :
+def plot_subtree(tree, depth_tree, min_width, offset, x, depth, ax):
+    font_size = 5
+    if depth < 3 :
         font_size = 10
-    elif current_depth < 6 :
-        font_size = 8
-    if current_depth > max_depth:
-        return
+    if depth < 10 :
+        font_size = 6
 
     if is_leaf_node(tree):
         # Plot a Leaf Label
-        plt.text(x, current_depth, "Label: " + str(tree['value']),
+        plt.text(x, depth, "Label: " + str(tree['value']),
                 fontsize=font_size,
                 bbox = dict(facecolor=get_label_color(tree['value']), alpha=1, boxstyle='round'),
                 ha='center', va='center')
     else:
         # Plot a Node Label
-        plt.text(x, current_depth, "X" + str(tree['attribute']) + " > " + str(tree['value']),
+        plt.text(x, depth, "X" + str(tree['attribute']) + " > " + str(tree['value']),
                 fontsize=font_size,
                 bbox = dict(facecolor=get_attr_color(tree['attribute']), alpha=1, boxstyle='round'),
                 ha='center', va='center')
-        # Plot 2 lines L and R
-        n = current_depth - max_depth
-        offset = pow(2, -n - 2) * min_width
-        l = x - offset
-        r = x + offset
-        plt.plot(np.array([x, l]), np.array([current_depth, current_depth + 1]), 'b')
-        plt.plot(np.array([x, r]), np.array([current_depth, current_depth + 1]), 'r')
+        # Plot 2 lines L and R based on depth of subtrees
+        ratio = depth_tree['left']['value'] / (depth_tree['left']['value'] + depth_tree['right']['value'])
+        l = x - ratio * offset
+        r = x + (1 - ratio) * offset
+        # Let chains of Leaf Nodes appear close together
+        if (is_leaf_node(depth_tree['right'])):
+            l = x - min(min_width, 0.5 * offset)
+        if (is_leaf_node(depth_tree['left'])):
+            r = x + min(min_width, 0.5 * offset)
+        
+        plt.plot(np.array([x, l]), np.array([depth, depth + 1]), 'b')
+        plt.plot(np.array([x, r]), np.array([depth, depth + 1]), 'r')
         # Recursively plot subtrees
-        plot_subtree(tree['left'], min_width, max_depth, l,  current_depth + 1, ax)
-        plot_subtree(tree['right'], min_width, max_depth, r, current_depth + 1, ax)
+        plot_subtree(tree['left'], depth_tree['left'], min_width, ratio * offset, l,  depth + 1, ax)
+        plot_subtree(tree['right'], depth_tree['right'], min_width, (1 - ratio) * offset, r, depth + 1, ax)
 
 if __name__ == "__main__":
     tree = {}
